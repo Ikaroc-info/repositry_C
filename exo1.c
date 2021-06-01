23a5236f087df205a39e1237074037fc841ee0c1
Ikaroc-info <leobertrand641@gmail.com>
2021-06-01 15:52:37 +0200

    exo1.c

--- exo1.c
+++ exo1.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+int entiers()
+{
+	for (int i=0; i<10;i++)
+	{
+		printf("nombre %d \n",i);
+	}
+}
+
 int main()
 {
 	printf("Hello wolrd\n");
 	int a = 5;
 	printf("%d\n",a);
+	entiers();
 }
+
+
 
